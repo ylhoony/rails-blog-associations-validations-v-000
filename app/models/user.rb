b985e1db13fe741cc582528242696fdf264287ff
Hoon Lee <yl.hoony@gmail.com>
2018-01-30 04:05:52 +0000

    automatically backed up by learn

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  has_many :posts
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 RailsBlog::Application.routes.draw do
 
+  resources :tags
+  resources :posts
+  resources :users
+
 end
